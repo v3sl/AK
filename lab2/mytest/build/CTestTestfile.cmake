59c2ee64edb0d576349dfba4061ced8847530606
Aleksandr Videvich <videvichs@gmail.com>
2023-03-25 12:52:09 +0300

    lab 2

---
+++ lab2/mytest/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/sasha/Рабочий стол/mpi/lab2/mytest
+# Build directory: C:/Users/sasha/Рабочий стол/mpi/lab2/mytest/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
